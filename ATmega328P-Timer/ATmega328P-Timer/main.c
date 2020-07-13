5f2717d7b6c442359eae4d77ed11c4f6f3773bb5
neilpatil-nz <57848464+neilpatil-nz@users.noreply.github.com>
2020-07-13 22:18:11 +1200

    1.3 Base Code
    Finished the base code.

--- ATmega328P-Timer/ATmega328P-Timer/main.c
+++ ATmega328P-Timer/ATmega328P-Timer/main.c
@@ -37,11 +37,23 @@
 #define CLR_LED_4   PORTD |= (1<<PD7)
 #define SET_LED_4   PORTD &= ~(1<<PD7)
 
+#define SET_DEFAULT_LED PORTB |= (1<<PB4)
+#define CLR_DEFAULT_LED PORTB &=~ (1<<PB4)
+
+#define SET_TIMER_LED PORTB |= (1<<PB3)
+#define CLR_TIMER_LED PORTB &=~ (1<<PB3)
+
+#define ADD_SWITCH_IS_HIGH PINB & (1<<PB5)
+#define ADD_SWITCH_IS_LOW ~PINB & (1<<PB5)
+
+#define MINUS_SWITCH_IS_HIGH PIND & (1<<PD0)
+#define MINUS_SWITCH_IS_LOW ~PIND & (1<<PD0)
+
 //Hardware macros for inputs
 //Hardware macros for ADC inputs
 
 /***** User macros *****/
-#define DEL_TIME 5
+#define DEL_TIME 2
 #define ALARM_DEL_TIME 100
 
 /***** States *****/
@@ -54,10 +66,11 @@
 
 /***** Declare & initialise global variables *****/
 
-uint8_t i =0;
-uint16_t count = 300;
-uint8_t state_clock = RUN;
-uint8_t state_setting = DEFAULT;
+uint8_t i = 0;
+volatile uint16_t count = 0;
+volatile uint16_t timer_count = 0;
+volatile uint8_t state_clock = RUN;
+volatile uint8_t state_setting = DEFAULT;
 
 /***** Interrupt Service Routines *****/
  ISR(TIMER1_COMPA_vect){
@@ -69,14 +82,34 @@ uint8_t state_setting = DEFAULT;
 	 if (state_clock == PAUSE){
 		 state_clock = RUN;
 	 }
-	 else{
-		 state_setting = DEFAULT;
+	 else if (state_setting == ALERT){
+		 state_clock = PAUSE;
+		 state_setting = TIMER;
+		 count = 0;
+	 }
+	 else {
 		 state_clock = PAUSE;
 	 }
 	 
  }
+ ISR(INT1_vect){
+	 if (state_setting == DEFAULT){
+		state_setting = TIMER;
+		count = 0;
+		CLR_DEFAULT_LED;
+		SET_TIMER_LED;
+	 }
+	 else{
+		 state_setting = DEFAULT;
+		 SET_DEFAULT_LED;
+		 CLR_TIMER_LED;
+	 }
+	 state_clock = PAUSE;
+	 count = 0;
+ }
  
 /***** Prototypes  for Functions *****/
+void disp_function();
 void call_num();
 void num_zero();
 void num_one();
@@ -104,13 +137,16 @@ int main(void) {
 	
 	DDRB |= (1 << PB1); //setup OC1A pin (B1) as output
 
+	
+	
+	
 	/***** External Interrupt Setup *****/
 	DDRD &=~ (1 << PD2);
 	PORTD |= (1 << PD2);//Interrupt0 
 	
-	//EICRA |= (1 <<  ISC00);
-	EICRA |= (1 <<  ISC01);
-	EIMSK |= (1 << INT0);
+	EICRA |= (1 <<  ISC00)|(1 <<  ISC01);
+	EICRA |= (1 <<  ISC11)|(1 <<  ISC10);
+	EIMSK |= (1 << INT0)|(1 << INT1);
 	
 	/***** Timer Config *****/
 	TCCR1B |= (1 << WGM12); // CTC mode
@@ -124,60 +160,99 @@ int main(void) {
 	DDRC = 0xff;
 	DDRB = 0xff;
 	
+	/***** Pin Inputs *****/
+	DDRB |= (1 << PB5);
+	PORTB |= (1<<PB5);
+	
+	DDRD &=~ (1<<PD0);
+	PORTD |= (1<<PD0);
+
+	
     // configure the internal pullup resistors for these pins
 
     /***** Main variables go here *****/
-
+	CLR_ALARM;
+	SET_DEFAULT_LED;
+	CLR_TIMER_LED;
     /***** Run once code goes here *****/
 
     /***** Loop code *****/
     while (1) {
-		CLR_ALARM;
-		if (count == 320){
-			state_clock = PAUSE;
-			state_setting = ALERT;
-			while(state_setting == ALERT){
-				SET_ALARM;
-				_delay_ms(ALARM_DEL_TIME);
-				CLR_ALARM;
-				_delay_ms(ALARM_DEL_TIME);
+		
+		if (state_setting == DEFAULT){
+			disp_function(count);
+		}
+		else if (state_setting == TIMER && state_clock == RUN) {
+			disp_function(count);
+			if (count == timer_count){
+				state_clock = PAUSE;
+				state_setting = ALERT;
+				while(state_setting == ALERT){
+					
+					SET_ALARM;
+					_delay_ms(ALARM_DEL_TIME);
+					CLR_ALARM;
+					_delay_ms(ALARM_DEL_TIME);
+				}
 			}
 		}
-		_delay_ms(DEL_TIME);
-		call_num(count/60);
-		CLR_LED_1;
-		CLR_LED_2;
-		SET_LED_3;
-		CLR_LED_4;
-		
-		
-		_delay_ms(DEL_TIME);
-		if ((count%60)/10 >= 6){
-			call_num(0);
-			CLR_LED_1;
-			SET_LED_2;
-			CLR_LED_3;
-			CLR_LED_4;
-		}
-		else {
-			call_num((count%60)/10);
-			CLR_LED_1;
-			SET_LED_2;
-			CLR_LED_3;
-			CLR_LED_4;
+		else if (state_setting == TIMER && state_clock == PAUSE){
+			disp_function(timer_count);
+			if (ADD_SWITCH_IS_LOW){
+				timer_count++;
+				disp_function(timer_count);
+			}
+			else if (MINUS_SWITCH_IS_LOW && timer_count !=0){
+				timer_count--;
+				disp_function(timer_count);
+			}
+			
 		}
 		
-	
-		_delay_ms(DEL_TIME);
-		call_num(count%10);
-		SET_LED_1;
-		CLR_LED_2;
-		CLR_LED_3;
-		CLR_LED_4;
 		
     } //while end
 } //main end
 /***** Functions *****/
+void disp_function(uint16_t count_input){
+	_delay_ms(DEL_TIME);
+	call_num(count_input / 600);
+	CLR_LED_1;
+	CLR_LED_2;
+	CLR_LED_3;
+	SET_LED_4;
+	
+	
+	_delay_ms(DEL_TIME);
+	call_num((count_input % 600)/60);
+	CLR_LED_1;
+	CLR_LED_2;
+	SET_LED_3;
+	CLR_LED_4;
+	
+	
+	_delay_ms(DEL_TIME);
+	if ((count_input % 60)/10 >= 6){
+		call_num(0);
+		CLR_LED_1;
+		SET_LED_2;
+		CLR_LED_3;
+		CLR_LED_4;
+	}
+	else {
+		call_num((count_input % 60)/10);
+		CLR_LED_1;
+		SET_LED_2;
+		CLR_LED_3;
+		CLR_LED_4;
+	}
+	_delay_ms(DEL_TIME);
+	call_num(count_input % 10);
+	SET_LED_1;
+	CLR_LED_2;
+	CLR_LED_3;
+	CLR_LED_4;
+}
+
 void call_num(uint16_t count_input){
 	switch (count_input)
 	{
@@ -224,41 +299,45 @@ void call_num(uint16_t count_input){
 
 void num_zero(){
 	PORTC = 0b0101111;
-	PORTB = 0b11111;
+	PORTB |= (1 << PB0)|(1 << PB2);
 }
 void num_one(){
 	PORTC = 0b0000100;
-	PORTB = 0b11111;
+	PORTB |= (1 << PB0)|(1 << PB2);
 }
 void num_two(){
 	PORTC = 0b111010;
-	PORTB = 0b11111;
+	PORTB |= (1 << PB0)|(1 << PB2);
 }
 void num_three(){
 	PORTC = 0b111100;
-	PORTB = 0b11111;
+	PORTB |= (1 << PB0)|(1 << PB2);
 }
 void num_four(){
 	PORTC = 0b0010101;
-	PORTB = 0b11111;
+	PORTB |= (1 << PB0)|(1 << PB2);
 }
 void num_five(){
 	PORTC = 0b111101;
-	PORTB = 0b11110;
+	PORTB &=~ (1 << PB0);
+	PORTB |=  (1 << PB2);
 }
 void num_six(){
 	PORTC = 0b111111;
-	PORTB = 0b11110;
+	PORTB &=~ (1 << PB0);
+	PORTB |=  (1 << PB2);
 }
 void num_seven(){
 	PORTC = 0b100100;
-	PORTB = 0b11111;
+	PORTB |= (1 << PB0)|(1 << PB2);
 }
 void num_eight(){
 	PORTC = 0b111111;
-	PORTB = 0b11111;
+	PORTB |= (1 << PB0)|(1 << PB2);
+
 }                                                                       
 void num_nine(){
 	PORTC = 0b110101;
-	PORTB = 0b11111;
+	PORTB |= (1 << PB0)|(1 << PB2);
+
 }
